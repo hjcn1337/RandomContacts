09ace0532da9019c21e94815e97d32e8b5214d00
Ростислав Ермаченков <errosgen@yandex.ru>
2021-03-05 16:45:43 +0300

    Project created

---
+++ RandomContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomContacts
+//
+//  Created by Ростислав Ермаченков on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
